aa6b980e6bab208770919577b22ed33facd433b9
MrArnav69 <arnavgupta6969@icloud.com>
2025-08-30 16:41:16 +0545

    Ptrns

---
+++ 1 to N Triangle Printer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1 to N Triangle Printer
+//
+//  Created by Arnav Gupta on 30/08/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Simple Right Angled Triangle Pattern/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Simple Right Angled Triangle Pattern
+//
+//  Created by Arnav Gupta on 30/08/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
